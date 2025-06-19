81a002a4e086b0029b7d9e5854ee73350a3cbc36
Arthur <71463136+Arthur-A-M@users.noreply.github.com>
2025-06-19 19:46:27 -0300

    Center the elements in the screen

--- app/page.tsx
+++ app/page.tsx
@@ -2,17 +2,17 @@ export default function Home() {
   return (
     <div>
       <header>
-        <nav>
+        <nav className="flex justify-center">
           <a href="/" target="_blank" rel="noopener noreferrer">link1</a>
           <a href="/" target="_blank" rel="noopener noreferrer">link2</a>
           <a href="/" target="_blank" rel="noopener noreferrer">link3</a>
         </nav>
       </header>
       <main>
-        <p>main</p>
+        <p className="text-center">main</p>
       </main>
       <footer>
-        <p>Footer</p>
+        <p className="text-center">Footer</p>
       </footer>
     </div>
   );
