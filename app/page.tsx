740138c7e0b3ced40985ff20721d3fd53cfba7d5
Arthur <71463136+Arthur-A-M@users.noreply.github.com>
2025-06-19 19:36:17 -0300

    Create header
    Header with a nav and mock up data was added.

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,13 @@
 export default function Home() {
   return (
     <div>
+      <header>
+        <nav>
+          <a href="/" target="_blank" rel="noopener noreferrer">link1</a>
+          <a href="/" target="_blank" rel="noopener noreferrer">link2</a>
+          <a href="/" target="_blank" rel="noopener noreferrer">link3</a>
+        </nav>
+      </header>
       <main>
         <p>main</p>
       </main>
