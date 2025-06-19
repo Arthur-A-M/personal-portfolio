4dc5dfb1995f106368997ed94376cf79e5f0a030
Arthur <71463136+Arthur-A-M@users.noreply.github.com>
2025-06-19 19:50:16 -0300

    Add the div that will be the hero

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
+import Image from "next/image";
+
 export default function Home() {
   return (
     <div>
@@ -9,6 +11,21 @@ export default function Home() {
         </nav>
       </header>
       <main>
+        <div>
+          <div className="flex justify-center">
+            <Image
+              src="/vercel.svg"
+              alt="Vercel Logo"
+              className="dark:invert"
+              width={100}
+              height={24}
+              priority
+            />
+          </div>
+          <h3 className="text-center">Name</h3>
+          <h5 className="text-center">Title</h5>
+          <p className="text-center">Sub title</p>
+        </div>
         <p className="text-center">main</p>
       </main>
       <footer>
