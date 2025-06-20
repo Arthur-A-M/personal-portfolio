758ec124160cdc0cd7f8d0e53b11aa878f6a4ead
Arthur <71463136+Arthur-A-M@users.noreply.github.com>
2025-06-20 07:05:42 -0300

    Add icons
    Footer now has icons referring to the links.

--- Components/Footer/index.tsx
+++ Components/Footer/index.tsx
@@ -1,12 +1,24 @@
+import Image from "next/image";
+
 import styles from "./footer.module.css";
+
+import LinkedIn from "@/public/linkedin.svg";
+import GitHub from "@/public/github.svg";
+import Gmail from "@/public/gmail.svg";
 
 export default function Footer() {
     return (
         <footer>
             <nav id="socials" className={styles.footerNav}>
-                <a href="/" target="_blank" rel="noopener noreferrer">Linked in</a>
-                <a href="/" target="_blank" rel="noopener noreferrer">GitHub</a>
-                <a href="/" target="_blank" rel="noopener noreferrer">Email</a>
+                <a href="/" target="_blank" rel="noopener noreferrer">
+                    <Image src={LinkedIn} alt="LinkedIn" width={24} height={24} />
+                </a>
+                <a className={styles.linkedIn} href="/" target="_blank" rel="noopener noreferrer">
+                    <Image src={GitHub} alt="GitHub" width={24} height={24} />
+                </a>
+                <a href="/" target="_blank" rel="noopener noreferrer">
+                    <Image src={Gmail} alt="Gmail" width={24} height={24} />
+                </a>
             </nav>
         </footer>
     );
