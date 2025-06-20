68000f6c8fa7294fab04ded12d4ea270303a384e
Arthur <71463136+Arthur-A-M@users.noreply.github.com>
2025-06-20 07:30:46 -0300

    Complete the links
    Links now have href, styles and tittle.

--- Components/Footer/index.tsx
+++ Components/Footer/index.tsx
@@ -9,15 +9,16 @@ import Gmail from "@/public/gmail.svg";
 export default function Footer() {
     return (
         <footer>
+            <h3 className={styles.tittle}>Socials</h3>
             <nav id="socials" className={styles.footerNav}>
-                <a href="/" target="_blank" rel="noopener noreferrer">
-                    <Image src={LinkedIn} alt="LinkedIn" width={24} height={24} />
+                <a href="https://www.linkedin.com/in/arthur-mendes-2a0686229/" target="_blank" rel="noopener noreferrer">
+                    <Image src={LinkedIn} alt="LinkedIn" width={50} height={50} />
                 </a>
-                <a className={styles.linkedIn} href="/" target="_blank" rel="noopener noreferrer">
-                    <Image src={GitHub} alt="GitHub" width={24} height={24} />
+                <a className={styles.linkedIn} href="https://github.com/Arthur-A-M" target="_blank" rel="noopener noreferrer">
+                    <Image src={GitHub} alt="GitHub" width={50} height={50} />
                 </a>
-                <a href="/" target="_blank" rel="noopener noreferrer">
-                    <Image src={Gmail} alt="Gmail" width={24} height={24} />
+                <a href="mailto:mendesarthur5656@gmail.com" target="_blank" rel="noopener noreferrer">
+                    <Image src={Gmail} alt="Gmail" width={50} height={50} />
                 </a>
             </nav>
         </footer>
