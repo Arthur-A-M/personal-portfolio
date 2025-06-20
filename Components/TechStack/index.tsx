75e1043c5398739c527f1d5f0c7374e0f7114166
Arthur <71463136+Arthur-A-M@users.noreply.github.com>
2025-06-20 09:32:20 -0300

    Create tech stack component with all techs

--- Components/TechStack/index.tsx
+++ Components/TechStack/index.tsx
@@ -1,13 +1,20 @@
+import Image from "next/image";
+
+import { TechStackIcons } from "@/public/TechStack";
+
 import styles from "./techStack.module.css";
 
 export default function TechStack() {
     return (
-    <section id="techstack" className={styles.techStack}>
-        <h3>tech stack</h3>
-        <ul>
-            <li>tech1</li>
-            <li>tech2</li>
-            <li>tech3</li>
-        </ul>
-    </section>);
+        <section id="techstack" className={styles.techStack}>
+            <h3>tech stack</h3>
+            <ul>
+                {Object.keys(TechStackIcons).map(iconName => (
+                    <li key={iconName}>
+                        <Image src={TechStackIcons[iconName]} alt={iconName} width={50} height={50} />
+                    </li>
+                ))}
+            </ul>
+        </section>
+    );
 }
---
+++ public/TechStack/index.ts
@@ -0,0 +1,23 @@
+import androidStudio from "./Icons/android-studio.svg";
+import expo from "./Icons/expo.svg";
+import HTML from "./Icons/html-5.svg";
+import CSS from "./Icons/css.svg";
+import NodeJS from "./Icons/nodejs.svg";
+import React from "./Icons/react.svg";
+import TypeScript from "./Icons/typescript.svg";
+import VSCode from "./Icons/vscode.svg";
+import git from "./Icons/git.svg";
+import windsurf from "./Icons/windsurf.svg";
+
+export const TechStackIcons: { [key: string]: any } = {
+    androidStudio,
+    expo,
+    HTML,
+    CSS,
+    NodeJS,
+    React,
+    TypeScript,
+    VSCode,
+    git,
+    windsurf,
+};
