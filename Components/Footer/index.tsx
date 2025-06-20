05b4ac4092c34bd84eb598aced94fd3aaeaac669
Arthur <71463136+Arthur-A-M@users.noreply.github.com>
2025-06-20 07:43:10 -0300

    Clean up the code
    Separate data into a variable, clear typos and make the nav more generative.

--- Components/Footer/index.tsx
+++ Components/Footer/index.tsx
@@ -7,19 +7,41 @@ import GitHub from "@/public/github.svg";
 import Gmail from "@/public/gmail.svg";
 
 export default function Footer() {
+    const iconSize = 50;
+
+    const icons: {
+        name: string;
+        href: string;
+        image: any;
+        className?: string;
+    }[] = [
+        {
+            name: "LinkedIn",
+            href: "https://www.linkedin.com/in/arthur-mendes-2a0686229/",
+            image: LinkedIn,
+        },
+        {
+            name: "GitHub",
+            href: "https://github.com/Arthur-A-M",
+            image: GitHub,
+            className: styles.gitHub,
+        },
+        {
+            name: "Gmail",
+            href: "mailto:mendesarthur5656@gmail.com",
+            image: Gmail,
+        },
+    ];
+
     return (
-        <footer>
-            <h3 className={styles.tittle}>Socials</h3>
+        <footer className={styles.footer}>
+            <h3 className={styles.title}>Socials</h3>
             <nav id="socials" className={styles.footerNav}>
-                <a href="https://www.linkedin.com/in/arthur-mendes-2a0686229/" target="_blank" rel="noopener noreferrer">
-                    <Image src={LinkedIn} alt="LinkedIn" width={50} height={50} />
-                </a>
-                <a className={styles.linkedIn} href="https://github.com/Arthur-A-M" target="_blank" rel="noopener noreferrer">
-                    <Image src={GitHub} alt="GitHub" width={50} height={50} />
-                </a>
-                <a href="mailto:mendesarthur5656@gmail.com" target="_blank" rel="noopener noreferrer">
-                    <Image src={Gmail} alt="Gmail" width={50} height={50} />
-                </a>
+                {icons.map(({ name, href, image, className }) => (
+                    <a key={name} href={href} target="_blank" rel="noopener noreferrer" className={className}>
+                        <Image src={image} alt={name} width={iconSize} height={iconSize} />
+                    </a>
+                ))}
             </nav>
         </footer>
     );
