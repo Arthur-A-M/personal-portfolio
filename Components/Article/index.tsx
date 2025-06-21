b68a8690cdee7d4a2748cb4accad825ec86e406b
Arthur <71463136+Arthur-A-M@users.noreply.github.com>
2025-06-21 16:52:54 -0300

    Add non mock up paragraph and style it

--- Components/Article/index.tsx
+++ Components/Article/index.tsx
@@ -3,9 +3,15 @@ import styles from "./article.module.css";
 export default function Article() {
     return (
         <article id="about" className={styles.article}>
-            <p>Paragraph 1</p>
-            <p>Paragraph 2</p>
-            <p>Paragraph 3</p>
+            <h3 className={styles.title}>Sobre mim</h3>
+            <p className={styles.paragraph}>Como desenvolvedor, 
+                minha filosofia é sempre adicionar valor, 
+                por meio de projetos que oferecem aplicações praticas para o usuário.
+                Sempre entendendo tanto a necessidade, a dor do cliente, como a solução, 
+                o produto em nível técnico; 
+                evitando acidentes e uso desnecessários de recursos.
+                Como estudante, acredito que o profissional deve ser T shaped. 
+                Entendendo o contexto no qual está envolvida sua habilidade prática e buscando constante aprimoramento. </p>
         </article>
     );
 }
