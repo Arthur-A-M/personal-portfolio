3109979d16e962a562c921715392f64b6a435b1a
Arthur <71463136+Arthur-A-M@users.noreply.github.com>
2025-06-22 10:59:02 -0300

    Add photo and style it

--- Components/Hero/index.tsx
+++ Components/Hero/index.tsx
@@ -2,22 +2,23 @@ import Image from "next/image";
 
 import styles from "./hero.module.css";
 
+import Photo from '@/public/photo.jpeg';
+
 export default function Hero(): React.ReactElement {
     return (
     <div className={styles.hero}>
-        <div>
+        <div className={styles.photoContainer}>
             <Image
-                src="/vercel.svg"
-                alt="Vercel Logo"
-                className="dark:invert"
-                width={100}
-                height={24}
+                src={Photo}
+                alt="Minha foto"
+                className={styles.photo}
+                width={200}
+                height={200}
                 priority
             />
         </div>
-        <h3>Name</h3>
-        <h5>Title</h5>
-        <p>Sub title</p>
+        <h3 className={styles.title}>Arthur</h3>
+        <h5 className={styles.subtitle}>Front-End Developer</h5>
     </div>
     );
 };
